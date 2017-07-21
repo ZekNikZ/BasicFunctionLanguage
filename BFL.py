eda298cff040ae039f3bd0b57dcb4101218c8307
GamrCorps <GamrCorps@gmail.com>
2017-07-20 22:46:01 -0700

    Fixed an issue with the filteroutput mode.

--- BFL.py
+++ BFL.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     debug = False
-    filter_none = False
+    filter_output = False
     i = 1
     while i < len(sys.argv):
         if sys.argv[i] == '-help' or sys.argv[i] == '-?':
@@ -32,19 +32,19 @@ if __name__ == '__main__':
                 code = data.split('\n', 1)
                 first_line = code[0]
 
-                prev_flags = (debug, filter_none)
+                prev_flags = (debug, filter_output)
                 if first_line.startswith('$'):
                     if 'debugmode' in first_line:
                         debug = True
                     if 'filteroutput' in first_line:
-                        filter_none = True
+                        filter_output = True
 
                 lexer = Lexer(code[1] if first_line.startswith('$') else data, debug)
                 token_list = lexer.run()
 
                 parser = Parser(token_list, debug)
                 result = parser.run()
-                if filter_none:
+                if filter_output:
                     result = list(filter(lambda x: x is not None, result))
                 print('[ANS] ' + str(result))
 
@@ -52,14 +52,14 @@ if __name__ == '__main__':
                     if 'debugmode' in first_line:
                         debug = prev_flags[0]
                     if 'filteroutput' in first_line:
-                        filter_none = prev_flags[0]
+                        filter_output = prev_flags[0]
 
                 i += 2
         elif sys.argv[i] == '-debug' or sys.argv[i] == '-d':
             debug = True
             i += 1
         elif sys.argv[i] == '-filteroutput' or sys.argv[i] == '-fo':
-            debug = True
+            filter_output = True
             i += 1
         else:
             print('ERROR: Invalid flag: %s' % sys.argv[i])
